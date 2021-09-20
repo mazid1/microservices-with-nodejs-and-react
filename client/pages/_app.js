f676d3a51b96008c27450837099def35d00c6f3a
Mazedul Islam <mazidmailbox@gmail.com>
2021-09-21 01:36:06 +0600

    [client] header and getInitialProps in AppComponent

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,24 @@
 import "bootstrap/dist/css/bootstrap.css";
-
+import App from "next/app";
 import React from "react";
+import buildClient from "../api/build-client";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <>
+      <h1>Header! {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  const httpClient = buildClient(appContext.ctx);
+  const { data } = await httpClient.get("/api/users/currentuser");
+
+  return { ...appProps, ...data };
+};
+
+export default AppComponent;
