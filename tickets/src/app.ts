5f2f2db09c4e4b9cff97f71710c6c78a7bf32508
Mazedul Islam <mazidmailbox@gmail.com>
2021-09-27 22:28:36 +0600

    [tickets] new ticket api implementation

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@mzdtickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@mzdtickets/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -12,6 +13,9 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
